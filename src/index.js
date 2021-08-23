f58bd06ee3719ddd4c368b296bfcfa3b40986320
vladKB1 <kazlou.uladzislau@gmail.com>
2021-08-23 21:41:03 +0300

    feat: task is done

--- src/index.js
+++ src/index.js
@@ -38,9 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    s = expr.split(/(.{10})/);    
+
+    let ans = "";
+
+    for (let i = 0; i < s.length; i++) if (s[i].length != 0) {        
+        if (s[i][0] == "*") {
+            ans += " ";
+            continue;
+        }
+        let letter = "";
+        for (let j = s[i].length - 1; j >= 1; j -= 2) {
+            if (s[i][j] == "1" && s[i][j - 1] == "1") {
+                letter = "-" + letter;
+            } else if (s[i][j] == "0" && s[i][j - 1] == "1") {
+                letter = "." + letter;
+            }
+        }    
+        
+        ans += MORSE_TABLE[letter];
+    }
+
+    return ans;
 }
 
 module.exports = {
     decode
 }
+
+console.log(decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"));
